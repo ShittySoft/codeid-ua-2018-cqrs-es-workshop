34e42fcb71e724dcfec71f756ee01246dc8f2014
Marco Pivetta <ocramius@gmail.com>
2018-08-12 17:18:44 +0200

    Moved projection into a service definition

--- bin/checked-in-users.php
+++ bin/checked-in-users.php
@@ -5,45 +5,7 @@ declare(strict_types=1);
 
 namespace Building\Projector;
 
-use Building\Domain\Aggregate\Building;
-use Building\Domain\DomainEvent\UserCheckedIn;
-use Building\Domain\DomainEvent\UserCheckedOut;
-use Interop\Container\ContainerInterface;
-use Prooph\EventSourcing\AggregateChanged;
-use Prooph\EventStore\EventStore;
-use Prooph\EventStore\Stream\StreamName;
-
 (function () {
-    /** @var ContainerInterface $container */
-    $container = require __DIR__ . '/../container.php';
-
-    $eventStore = $container->get(EventStore::class);
-
-    /** @var AggregateChanged[] $allEvents */
-    $allEvents = $eventStore->loadEventsByMetadataFrom(
-        new StreamName('event_stream'),
-        ['aggregate_type' => Building::class]
-    );
-
-    $usersPerBuilding = [];
-
-    foreach ($allEvents as $event) {
-        $buildingId = $event->aggregateId();
-        $usersPerBuilding[$buildingId] = $usersPerBuilding[$buildingId] ?? [];
-
-        if ($event instanceof UserCheckedIn) {
-            $usersPerBuilding[$buildingId][$event->username()] = true;
-        }
-
-        if ($event instanceof UserCheckedOut) {
-            unset($usersPerBuilding[$buildingId][$event->username()]);
-        }
-    }
-
-    array_walk($usersPerBuilding, function (array $users, string $buildingId) : void {
-        file_put_contents(
-            __DIR__ . '/../public/' . $buildingId . '.json',
-            json_encode(array_keys($users))
-        );
-    });
+    (require __DIR__ . '/../container.php')
+        ->get('project-checked-in-users')();
 })();
--- container.php
+++ container.php
@@ -249,5 +249,39 @@ return new ServiceManager([
                 )
             );
         },
+        'project-checked-in-users' => function (ContainerInterface $container) {
+            $eventStore = $container->get(EventStore::class);
+
+            return function () use ($eventStore) : void {
+
+                /** @var AggregateChanged[] $allEvents */
+                $allEvents = $eventStore->loadEventsByMetadataFrom(
+                    new StreamName('event_stream'),
+                    ['aggregate_type' => Building::class]
+                );
+
+                $usersPerBuilding = [];
+
+                foreach ($allEvents as $event) {
+                    $buildingId                    = $event->aggregateId();
+                    $usersPerBuilding[$buildingId] = $usersPerBuilding[$buildingId] ?? [];
+
+                    if ($event instanceof UserCheckedIn) {
+                        $usersPerBuilding[$buildingId][$event->username()] = true;
+                    }
+
+                    if ($event instanceof UserCheckedOut) {
+                        unset($usersPerBuilding[$buildingId][$event->username()]);
+                    }
+                }
+
+                array_walk($usersPerBuilding, function (array $users, string $buildingId) : void {
+                    file_put_contents(
+                        __DIR__ . '/../public/' . $buildingId . '.json',
+                        json_encode(array_keys($users))
+                    );
+                });
+            };
+        },
     ],
 ]);
